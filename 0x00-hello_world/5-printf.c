5c14660d7f49ced9feea906d78ed13c5e9951e3f
Peter Okorafor <peterdtitan@gmail.com>
2020-09-16 18:17:47 +0000

    fixed betty issue for 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* manin - Prints a string
+* main - Prints a string
 *
 * Description: Prints "with proper grammar, but the outcome is a piece of art"
 *
@@ -9,6 +9,6 @@
 */
 int main(void)
 {
-	printf("%s" , "with proper grammar, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
