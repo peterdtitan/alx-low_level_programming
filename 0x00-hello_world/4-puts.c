83b827af2f5a9e838da4680b02950ca450dea012
Peter Okorafor <peterdtitan@gmail.com>
2020-09-16 14:59:37 +0000

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Prints a string
+* Descritption: Prints "\"Programming is like building a multilingual puzzle" 
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
