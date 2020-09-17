3ac8bf3a92c931ed99771670244941382cde533f
Peter <peterdtitan@gmail.com>
2020-09-17 19:19:59 +0000

    update

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+* main - create pairs of numbers without repetition
+* Return: 0
+*/
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i; j < 58; j++)
+		{
+
+			for (k = j; k < 58; k++)
+			{
+				for (l = k; l < 58; l++)
+				{
+					if (i == j == k == l)
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+
+					putchar(',');
+					putchar(' ');
+
+				}
+			}
+		}
+	}
+	return (0);
+}
