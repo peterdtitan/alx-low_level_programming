d147aaef7c284566b37e724a3631e53ee8b8e64b
Peter Okorafor <peterdtitan@gmail.com>
2023-08-12 06:33:13 +0300

    Solved challenges

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints numbers below 10
+* no use of char variables
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Project two focusing on "if, else, while" other loops.
